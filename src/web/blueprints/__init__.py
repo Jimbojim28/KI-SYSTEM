766ad5c39d409642ab42862e1885c8f5f81ccb0b
SHP-ART <SHP-ART@users.noreply.github.com>
2026-01-03 23:15:14 +0100

    fix: Füge bathroom_bp zu blueprints __init__.py hinzu

--- src/web/blueprints/__init__.py
+++ src/web/blueprints/__init__.py
@@ -10,16 +10,19 @@ from .validators import validate_request, validate_json_body, FieldValidator, Va
 from .api_config import config_bp, init_config_blueprint
 from .api_ml import ml_bp, init_ml_blueprint
 from .api_ventilation import ventilation_bp, init_ventilation_blueprint
+from .api_bathroom import bathroom_bp, init_bathroom_blueprint
 
 __all__ = [
     # Blueprints
     'config_bp',
     'ml_bp',
     'ventilation_bp',
+    'bathroom_bp',
     # Init-Funktionen
     'init_config_blueprint',
     'init_ml_blueprint',
     'init_ventilation_blueprint',
+    'init_bathroom_blueprint',
     # Validators
     'validate_request',
     'validate_json_body',
